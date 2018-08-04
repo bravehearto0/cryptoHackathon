4a16343e371cd6ca3c541c7706fb0514fa6a57ed
Xiao Chen <xiaoc@uber.com>
2018-08-04 16:49:54 -0700

    fix

---
+++ geoViewer/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+var dirname = process.cwd()
+
+app.get('/health', (req, res) => res.send('OK'));
+app.use(express.static('static'));
+app.use(express.static('data'));
+app.get('/pokey', function(req, res) {
+    res.sendFile(dirname + '/static/pokey.html');
+});
+
+app.listen(3000);
