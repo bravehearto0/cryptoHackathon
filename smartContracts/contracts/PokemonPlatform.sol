85674ab6ab43ba180fc4435013ec04e3b8200da3
gongf05 <fang.ucla@gmail.com>
2018-08-04 15:44:42 -0700

    ss

--- smartContracts/contracts/PokemonPlatform.sol
+++ smartContracts/contracts/PokemonPlatform.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.22;
-import "./ownable.sol";
 
-contract PokemonPlatform is Ownable{
+import './zeppelin/Ownable.sol';
+
+contract PokemonPlatform is Ownable {
     // Parameters of the Pokemon
     struct Pokemon { // Struct
         string name;
@@ -11,6 +12,9 @@ contract PokemonPlatform is Ownable{
         uint32 releaseTimestamp;
     }
 
+    mapping (uint => Pokemon) public mPokemon;
+
+    uint currentGeneration;
     // latitudeInt is the integer part of the latitude, latitudeFloat is the float part
     struct Access {
       uint timestamp;
@@ -28,18 +32,18 @@ contract PokemonPlatform is Ownable{
 
     Pokemon [] allPokemons;
     // the ownership of the pokemonId  => owner address
-    mapping(uint => address) ownerToPokemon;
+    mapping(uint => address) public ownerToPokemon;
 
     // given address return the user profile info
     mapping(address => UserProfile) ownerToProfile;
     // store the location of pokemon, byte32 is the hash versioned of the geohash
-    mapping(byte32 => uint) lottery;
+    mapping(bytes32 => uint) public lottery;
 
     // the last access user have. used to GPS check and rate limit
-    mapping(address => Access) lastAccess;
+    mapping(address => Access) public lastAccess;
 
     // the generation number and total count
-    event NewPokemonGenerationReleased(uint generation, uint totalCount);
+    //event NewPokemonGenerationReleased(uint generation, uint totalCount);
 
     uint rateLimitInterval;
     uint currentGeneration;
@@ -54,27 +58,68 @@ contract PokemonPlatform is Ownable{
     }
 
     // TODO: apply ratelimit and GPS spoofing checks
-    function findPokemon(uint latitudeInt, uint latitudeFloat, uint longitudeInt, uint longitudeFloat) external returns(bool) {
+    //function findPokemon(uint latitudeInt, uint latitudeFloat, uint longitudeInt, uint longitudeFloat) external returns(bool) {
 
-    }
+    //}
 
-    // get the pokemon belong to current owner
-    function getMyPokemons() external view returns (Pokemon[]){
-
+    // get the pokemon belong to current owner - retuns integer array of Pokemon Ids
+    function getMyPokemons() external view returns (uint[]){
+      // loop through to look for Pokemon belonging to this msg.sender
+      uint[] arrayId;
+      for (uint i=0; i < allPokemons.length; i++) {
+          if(ownerToPokemon[i] == msg.sender){
+              arrayId.push(i);
+          }
+      }
+      // copy to fixed-size array
+      uint[] memory v = new uint[](arrayId.length);
+      for (i=0; i < arrayId.length; i++) {
+          v[i] = arrayId[i];
+      }
+      return v;
     }
 
     // get all available pokemon that has releaseTimestamp
-    function getAllPokemons() external view returns (Pokemon[]) {
+    function getAllPokemons() external view returns (uint[]) {
+      // find all released Pokemon
+      uint[] arrayId;
+      for (uint i=0; i < allPokemons.length; i++) {
+          // release time is earlier than current timestamp -> released Pokemon
+          if(allPokemons[i].releaseTimestamp != 0 ){
+              arrayId.push(i);
+          }
+      }
 
+      // copy to fixed-size array
+      uint[] memory v = new uint[](arrayId.length);
+      for (i=0; i < arrayId.length; i++) {
+          v[i] = arrayId[i];
+      }
+      return v;
     }
 
     // get the unclaimed pokemon mapping
-    function getUnclaimedPokemons() external view returns(Pokemon[]) {
+    function getUnclaimedPokemons() external view returns(uint[]) {
       // front end parse the generation and total count
+      // find all released Pokemon
+      uint[] arrayId;
+      for (uint i=0; i < allPokemons.length; i++) {
+          // release time is earlier than current timestamp -> released Pokemon
+          if(allPokemons[i].releaseTimestamp == 0){
+              arrayId.push(i);
+          }
+      }
+
+      // copy to fixed-size array
+      uint[] memory v = new uint[](arrayId.length);
+      for (i=0; i < arrayId.length; i++) {
+          v[i] = arrayId[i];
+      }
+      return v;
     }
 
-    // used to populate the list of pokemon
-    function populatePokemons(string [] pokemons) public onlyOwner {
+    // used to add new pokemons into allPokemons
+    function populatePokemons(uint[] pokemons) public onlyOwner() {
 
     }
 
@@ -90,6 +135,7 @@ contract PokemonPlatform is Ownable{
 
     // return the user getProfile
     function getProfile() external view returns (userProfile) {
+
 
     }
 
