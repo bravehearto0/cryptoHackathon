b01c744e12186afe6382e841963cc145a75aafa1
Joshua <zirho6@gmail.com>
2018-08-05 11:15:19 -0700

    refactor action

--- frontend/src/client/modules/home/reducer.js
+++ frontend/src/client/modules/home/reducer.js
@@ -4,15 +4,19 @@ import * as api from './api.js';
 const defaultState = { profile: null };
 
 // export actions
-export const getProfile = createAction('getProfile');
+export const getProfile = createAction('getProfile',
+  async () => {
+    const result = await api.getAllPokemons();
+    console.log('result', result);
+    return { result };
+  },
+);
 
 const reducer = handleAction(
   getProfile,
   async (state, { payload }) => {
-    const result = await api.getAllPokemons();
     console.log('state', state);
     console.log('payload', payload);
-    console.log('result', result);
     return state;
   },
   defaultState,
