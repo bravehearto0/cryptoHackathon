02cc1c4897529bf9fc0b86ae7180e990694c7b51
Victor Chen <victor.chen>
2018-08-05 09:52:59 -0700

    update api

--- frontend/src/client/modules/home/api.js
+++ frontend/src/client/modules/home/api.js
@@ -1,4 +1,18 @@
-export function getMyPokemons(contract) {
+// eslint-global web3
+
+import Web3 from 'web3'
+import abiFile from './PokemonPlatform.json';
+
+let web3js;
+let contract;
+const CONTRACT_ADDRESS = '0xe3bf2f428ede719dc1b6f2320f4558cba726cb66';
+
+export const init = () => {
+  web3js = new Web3(web3.currentProvider);
+  contract = new web3js.eth.Contract(abiFile.abi, CONTRACT_ADDRESS);
+};
+
+export function getMyPokemons() {
   return contract.methods.getMyPokemons().call()
   .then(function(ids) {
     return Promise.all(ids, function(id) {
@@ -51,3 +65,18 @@ export function getUnclaimedPokemons() {
     });
   });
 }
+
+export function getProfile() {
+  return contract.methods.getProfile().call()
+  .then(function([name, userAddress, photoUrl, numPokemon]) {
+    console.log(name);
+    return {
+      name,
+      userAddress,
+      photoUrl,
+      numPokemon,
+    };
+  });
+}
+
+init();
