cdfd8acdb051d4b3923928907162750f48399e0e
Joshua <zirho6@gmail.com>
2018-08-05 12:06:57 -0700

    update logging

--- frontend/src/client/modules/home/reducer.js
+++ frontend/src/client/modules/home/reducer.js
@@ -15,10 +15,10 @@ export const getProfile = createAction('getProfile',
 export const populatePokemon = createAction('populatePokemon',
   async () => {
     const result = await api.populatePokemon(
-      "Genesis",
-      "Greetings, human. I am Genesis. The dogs know me as alpha; the cats know me as omega. To your kind, I am a riddle wrapped in an enigma, first found by a user in Mystery, Alaska. I looked into the void and the void looked back. Then I lost interest. I can 2019t wait to be your new owner!",
-      "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1.png",
-      0
+      'Genesis',
+      'Greetings',
+      'https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/1.png',
+      '0'
     );
     return { result };
   },
@@ -34,6 +34,7 @@ export const releasePokemon = createAction('releasePokemon',
 export const getAllPokemons = createAction('getAllPokemons',
   async () => {
     const result = await api.getAllPokemons();
+    console.log('result', result);
     return { result };
   },
 );
